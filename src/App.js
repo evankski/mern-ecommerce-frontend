87fd84a13fcea638e413bd74f0f93b63d8d87c3b
evankski <evankarwowski@gmail.com>
2022-06-06 19:47:55 -0700

    creates product data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href='/'>Evazon</a>
+        <a href='/'>Athleticism</a>
       </header>
       <main>
         list products here
---
+++ src/data.js
@@ -0,0 +1,52 @@
+const data = {
+    products: [
+        {
+            name: 'Niki slim shirt',
+            slug: 'niki-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality nike slimfit shirt'
+        },
+        {
+            name: 'Adidas athletic fit shirt',
+            slug: 'adidas-athletic-fit-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 18,
+            description: 'Adidas performace shirt with an athletic fit'
+        },
+        {
+            name: 'Niki workout pants',
+            slug: 'niki-workout-pants',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 85,
+            countInStock: 7,
+            brand: 'Nike',
+            rating: 3.4,
+            numReviews: 43,
+            description: 'Sweat resistant workout pants'
+        },
+        {
+            name: 'Adidas workout pants',
+            slug: 'adidas-workout-pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 90,
+            countInStock: 27,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 39,
+            description: 'Sweat resistant workout pants'
+        }
+    ]
+}
